c6637f2a62883226c8f70b2965ab2a6df9b8b29a
Gjore Milevski <dzole0311@gmail.com>
2026-03-18 15:41:19 +0100

    build: add entry points and peer deps for maplibre and deck.gl

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,10 +10,38 @@ export default defineConfig([
     external: ['mapbox-gl', 'hls.js'],
   },
   {
+    entry: { maplibre: 'src/maplibre.ts' },
+    format: ['esm', 'cjs'],
+    dts: true,
+    sourcemap: true,
+    external: ['maplibre-gl', 'hls.js'],
+  },
+  {
+    entry: { deckgl: 'src/deckgl.ts' },
+    format: ['esm', 'cjs'],
+    dts: true,
+    sourcemap: true,
+    external: ['@deck.gl/core', '@deck.gl/layers', 'hls.js'],
+  },
+  {
     entry: { react: 'src/react.ts' },
     format: ['esm', 'cjs'],
     dts: true,
     sourcemap: true,
     external: ['mapbox-gl', 'hls.js', 'react', 'react-map-gl', 'react-map-gl/mapbox'],
+  },
+  {
+    entry: { 'react-maplibre': 'src/react-maplibre.ts' },
+    format: ['esm', 'cjs'],
+    dts: true,
+    sourcemap: true,
+    external: ['maplibre-gl', 'hls.js', 'react', 'react-map-gl', 'react-map-gl/maplibre'],
+  },
+  {
+    entry: { 'react-deckgl': 'src/react-deckgl.ts' },
+    format: ['esm', 'cjs'],
+    dts: true,
+    sourcemap: true,
+    external: ['@deck.gl/core', '@deck.gl/layers', 'hls.js', 'react'],
   },
 ]);
